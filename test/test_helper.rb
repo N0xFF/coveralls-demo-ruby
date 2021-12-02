4cb70e65868dc9c77e6b8eedd9af41ee55088899
Maxim Perepelitsa <n0xff@outlook.com>
2021-12-02 08:56:50 +0300

    Send parallel builds webhook
    https://docs.coveralls.io/parallel-build-webhook

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 require "simplecov"
+require "simplecov-lcov"
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.report_with_single_file = true
+  c.output_directory = "coverage"
+  c.lcov_file_name = "lcov.info"
+end
+
 SimpleCov.start do
   if ENV["CI"]
-    require "coveralls"
-    formatter Coveralls::SimpleCov::Formatter
+    formatter SimpleCov::Formatter::LcovFormatter
   else
     formatter SimpleCov::Formatter::HTMLFormatter
   end
